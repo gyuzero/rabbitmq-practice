481ec3395e8d2e62ed8f3fb43af4c5abef28584a
gyuzero <gyuzero0103@gmail.com>
2025-06-08 16:11:11 +0900

    initial commit

---
+++ src/test/java/com/gyuzero/rabbitmqpractice/RabbitmqPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gyuzero.rabbitmqpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitmqPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
